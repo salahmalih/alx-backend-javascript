753772001c5a01951409d5b62e5e8f630cfc5bd5
salahmalih <C130084552@taalim.ma>
2024-08-12 11:01:03 -0500

    task all

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,7 +2,7 @@
  * Changes the quantity of unique grocery items to 100.
  * @param {Map<String, number>} map - A map of the name of a
  * grocery and its quantity.
- * 
+ * @author Salah malih <https://github.com/salahmalih>
  */
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
