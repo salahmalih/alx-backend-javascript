adf8579051b0f73467c67a1f000a0871a82a03f3
salahmalih <C130084552@taalim.ma>
2024-09-30 14:59:48 -0500

    task all

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author SALAH MALIH <https://github.com/salahmalih>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
